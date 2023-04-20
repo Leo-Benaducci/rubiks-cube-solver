40670878caa6bf65b6303021b1a5141d6ae60774
Leo Benaducci <leobenaducci>
2023-04-20 13:39:34 -0300

    Initial commit

---
+++ src/main/java/br/com/lbenaducci/rubikscubesolver/RubiksCubeSolverApplication.java
@@ -0,0 +1,13 @@
+package br.com.lbenaducci.rubikscubesolver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RubiksCubeSolverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RubiksCubeSolverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lbenaducci/rubikscubesolver/RubiksCubeSolverApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lbenaducci.rubikscubesolver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RubiksCubeSolverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
